e41dc5f08901f42a43e8e9b24d71fb089bf7ab06
HarryPutt <putth@macstudent.com.au>
2023-03-03 09:09:26 +1100

    Initial Commit

---
+++ You Queue!/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  You Queue!
+//
+//  Created by Harrison Putt on 3/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ You Queue!/You_Queue_App.swift
@@ -0,0 +1,17 @@
+//
+//  You_Queue_App.swift
+//  You Queue!
+//
+//  Created by Harrison Putt on 3/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct You_Queue_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
