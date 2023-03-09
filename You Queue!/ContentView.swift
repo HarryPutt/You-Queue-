10a6795220a2860df4e1a7639ddc6e597ebb695f
HarryPutt <putth@macstudent.com.au>
2023-03-09 14:42:10 +1100

    Finished the teachers screen and created a queue for all the students waiting. I've also create a button to join the queue but it does not currently work. All buttons to go back to previous screens are working

--- You Queue!/ContentView.swift
+++ You Queue!/ContentView.swift
@@ -7,13 +7,100 @@
 
 import SwiftUI
 
+
+struct QueueView: View {
+    @State private var student = ""
+    var Teacher:String
+    var body: some View {
+        VStack {
+            Text("Chaitanya Reddy")
+                .padding(10)
+            Text("Benjamin Abdy")
+                .padding(10)
+            Text("Harris Awan")
+                .padding(10)
+            Text("Rowena Kite")
+                .padding(10)
+            Text("Alexandra Southern")
+                .padding(10)
+            Text("William Milne")
+                .padding(10)
+            Text("Joel Braatz")
+                .padding(10)
+            TextField("Enter your name here", text: $student)
+                .multilineTextAlignment(.center)
+                .padding(10)
+            Button(action: {
+                Text(student)
+            }, label: {
+                Text("Join Queue")
+            })
+            .padding(10)
+        }
+        .navigationBarTitle(Teacher + " - Current Queue", displayMode:.inline)
+    }
+}
+
+
 struct SubjectView: View {
-    @State private var teacher = "Joe Mama"
     var Subject:String
     var body: some View {
         NavigationView{
             VStack {
-                Text("Hello World")
+                NavigationLink(destination: QueueView(Teacher: "Mrs James")) {
+                    Text("Mrs James")
+                        .padding(10)
+                        .font(.title)
+                        .border(Color.yellow, width: 2)
+                        .background(Color.yellow)
+                    
+                        .padding(30)
+                }
+                NavigationLink(destination: QueueView(Teacher: "Ms Tor")) {
+                    Text("Ms Tor")
+                        .padding(10)
+                        .font(.title)
+                        .border(Color.red, width: 2)
+                        .background(Color.red)
+                    
+                        .padding(30)
+                }
+                NavigationLink(destination: QueueView(Teacher: "Mr Gorton")) {
+                    Text("Mr Gorton")
+                        .padding(10)
+                        .font(.title)
+                        .border(Color.cyan, width: 2)
+                        .background(Color.cyan)
+                    
+                        .padding(30)
+                }
+                NavigationLink(destination: QueueView(Teacher: "Mr Hordern")) {
+                    Text("Mr Hordern")
+                        .padding(10)
+                        .font(.title)
+                        .border(Color.green, width: 2)
+                        .background(Color.green)
+                    
+                        .padding(30)
+                }
+                NavigationLink(destination: QueueView(Teacher: "Mrs Cochrane")) {
+                    Text("Mrs Cochrane")
+                        .padding(10)
+                        .font(.title)
+                        .border(Color.orange, width: 2)
+                        .background(Color.orange)
+                    
+                        .padding(30)
+                }
+                NavigationLink(destination: QueueView(Teacher: "Miss Yang")) {
+                    Text("Miss Yang")
+                        .padding(10)
+                        .font(.title)
+                        .border(Color.mint, width: 2)
+                        .background(Color.mint)
+                    
+                        .padding(30)
+                }
             }
         }
     .navigationBarTitle(Subject, displayMode:.inline)
